8aefa7d5b5f7c2a6edbec1084c22104cda5af45a
Tom Lennox <dennkousekka1212@gmail.com>
2019-08-17 12:14:56 +0900

    【nandemo】環境構築

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <hr>
+    <table>
+      <th>
+      </th>
+      <tr>
+        <td>a</td>
+        <td>a</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: '*yarn ',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
