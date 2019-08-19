9c53de3cc4e5626af204976653cd2c6fdb2a8719
Tom Lennox <dennkousekka1212@gmail.com>
2019-08-19 23:33:14 +0900

    【nandemo】お金持ちな気分になれるアプリ作成

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,25 +2,52 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <hr>
-    <table>
+    <!-- <table>
       <th>
       </th>
-      <tr>
-        <td>a</td>
-        <td>a</td>
+      <tr v-for="weight in weight" :key="weight">
+        <td v-for="rm in rms" :key="rm.rm">
+          {{ rm }}
+        </td>
       </tr>
-    </table>
+    </table> -->
+    <p style="text-align: left; padding-left: 40vw;">{{ p | calc }}円</p>
+    <pre style="text-align: left;">{{$data}}</pre>
   </div>
 </template>
 
 <script>
+import { setInterval } from 'timers';
+// import { setTimeout } from 'timers';
+// import axios from 'axios'
+
 export default {
   name: 'HelloWorld',
   data() {
     return {
-      msg: '*yarn ',
+      msg: '*',
+      // rms: [],
+      // weight: [],
+      reps: 50,
+      depth: 5,
+      p: 0,
+      second: 60 * 60 * 24 * 365 * 10,
+      money: 8000000,
+      secondPerMoney: 0,
+      interval: null,
     };
   },
+  created() {
+    this.secondPerMoney = this.money / this.second
+    this.interval = setInterval(() => {
+      this.p = this.p + this.secondPerMoney
+    }, 100)
+  },
+  filters: {
+    calc: function(v) {
+      return Math.round(v * 10000) / 10000
+    }
+  }
 };
 </script>
 
